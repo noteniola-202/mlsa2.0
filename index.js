cbf8279eb4085dbb11d1f5383eccd197d4974bb1
Eniola <alaoeuniceeniola@gmail.com>
2024-12-16 17:48:13 +0100

    added more features

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var heading = document.querySelector('h1');
 heading.addEventListener('click', function (){
     heading.innerHTML= "Feel Welcomed";
 });
+var bodyText= document.querySelector('p');
+bodyText.addEventListener('click', function (){
+    bodyText.innerHTML= "I said its EASY!";
+});
